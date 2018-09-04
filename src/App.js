f1ceb8cc43e1d247fdf94e0900f1f89b2776a9bd
Michele Di Salvatore <git@micheledisalvatore.it>
2018-09-04 12:48:39 -0500

    add notifications on coundown change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Notify from 'notifyjs';
 
 import Countdown from './components/countdown';
 import Controller from './components/controller';
@@ -9,7 +10,7 @@ import { Container } from './App.styled';
 const DURATION = {
   focus: 25 * 60,
   shortBreak: 5 * 60,
-  longBreak: 25 * 60,
+  longBreak: 30 * 60,
 };
 const SESSIONS = 4;
 const GOALS = 12;
@@ -60,7 +61,25 @@ class App extends Component {
       }
 
       return newState
+    }, () => {
+      if (!Notify.needsPermission) {
+        if(this.state.isTimeFocus) {
+          (new Notify('Focus', { body: `Time to be focused for 25 minutes (round ${this.state.currentSession} of ${SESSIONS})` })).show();
+        } else if (this.state.isTimeShortBreak) {
+          (new Notify('Short break', { body: `Time to take a break of 5 minutes (round ${this.state.currentSession} of ${SESSIONS})` })).show();
+        } else if (this.state.isTimeLongBreak) {
+          (new Notify('Long break', { body: `Time to relax for 30 minutes (goal ${this.state.currentGoal} of ${GOALS})` })).show();
+        }
+      }
     })
+  }
+
+  notifyAndToggle = () => {
+    if (Notify.needsPermission && Notify.isSupported()) {
+      Notify.requestPermission(this.toggleCountdown, this.toggleCountdown)
+    } else {
+      this.toggleCountdown();
+    }
   }
 
   toggleCountdown = () => {
@@ -82,7 +101,7 @@ class App extends Component {
         {this.state.isTimeFocus && <Countdown color="red" duration={DURATION.focus} onEnd={this.onCountdownEnd} isPaused={this.state.isPaused} pauseDuration={this.state.pauseDuration} />}
         {this.state.isTimeShortBreak && <Countdown color="blue" duration={DURATION.shortBreak} onEnd={this.onCountdownEnd} isPaused={this.state.isPaused} pauseDuration={this.state.pauseDuration} />}
         {this.state.isTimeLongBreak && <Countdown color="green" duration={DURATION.longBreak} onEnd={this.onCountdownEnd} isPaused={this.state.isPaused} pauseDuration={this.state.pauseDuration} />}
-        <Controller onClick={this.toggleCountdown} isPaused={this.state.isPaused} />
+        <Controller onClick={this.notifyAndToggle} isPaused={this.state.isPaused} />
         <div>
           <InfoBox current={this.state.currentSession} total={SESSIONS} title="Round" />
           <InfoBox current={this.state.currentGoal} total={GOALS} title="Goal" />
